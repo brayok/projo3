7cff354345f3104099b63abfedadfe19d31ebda3
brayok <selaciak@gmail.com>
2020-12-22 00:22:33 +0300

    added js code

--- js/script.js
+++ js/script.js
@@ -19,9 +19,14 @@ $(".click1").click(function () {
     $(".hide1").slideToggle(1000);
     $(".show1").slideToggle(1200);
   });
-  
+
   $(".click2").click(function () {
     $(".hide2").slideToggle(1000);
     $(".show2").slideToggle(1200);
+  });
+
+  $(".click3").click(function () {
+    $(".hide3").slideToggle(1000);
+    $(".show3").slideToggle(1200);
   });
   
