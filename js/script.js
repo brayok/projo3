2d26098d9b9161db483dfda50500788fa2e1c28f
brayok <selaciak@gmail.com>
2020-12-22 00:44:15 +0300

    js identation to 4 spaces

--- js/script.js
+++ js/script.js
@@ -2,45 +2,46 @@
 //     $("#design").click(function(){
 //       $(".design").toggle();
 //     });
-//   });
+// });
 
-//   $(document).ready(function(){
+// $(document).ready(function(){
 //     $("#development").click(function(){
-//       $(".development").toggle();
+//         $(".development").toggle();
 //     });
-//   });
+// });
 
-//   $(document).ready(function(){
+// $(document).ready(function(){
 //     $("#product").click(function(){
-//       $(".product").toggle();
+//         $(".product").toggle();
 //     });
-//   });
+// });
+
 $(".click1").click(function () {
     $(".hide1").slideToggle(1000);
     $(".show1").slideToggle(1200);
-  });
+});
 
-  $(".click2").click(function () {
+$(".click2").click(function () {
     $(".hide2").slideToggle(1000);
     $(".show2").slideToggle(1200);
-  });
+});
 
-  $(".click3").click(function () {
+$(".click3").click(function () {
     $(".hide3").slideToggle(1000);
     $(".show3").slideToggle(1200);
-  });
+});
 
-  $(function () {
+$(function () {
     $("form#form").on('submit', function (event) {
       event.preventDefault();
       let name = $("input#name").val();
 
-      if ($("input#name").val()) {
+        if ($("input#name").val()) {
         alert("Hey " + name + ", Thanks for reaching out to us,Will get back to you");
-      }
-      else {
+        }
+        else {
         alert("Please provide your correct name and email!");
-      }
+        }
   
     });
 
