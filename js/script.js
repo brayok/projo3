a7a481bfa21185e0a714e3bc1b105d15c78c98e0
brayok <selaciak@gmail.com>
2020-12-21 23:47:42 +0300

    added js code to js file

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//     $("#design").click(function(){
+//       $(".design").toggle();
+//     });
+//   });
+
+//   $(document).ready(function(){
+//     $("#development").click(function(){
+//       $(".development").toggle();
+//     });
+//   });
+
+//   $(document).ready(function(){
+//     $("#product").click(function(){
+//       $(".product").toggle();
+//     });
+//   });
